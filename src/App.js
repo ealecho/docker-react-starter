598f8972feb359973c51d193858603198ab3993f
ealecho <elshore32@gmail.com>
2021-10-04 10:55:42 +0300

    testing actions

--- src/App.js
+++ src/App.js
@@ -7,14 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi there!</p>
+        <p>I am working</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
